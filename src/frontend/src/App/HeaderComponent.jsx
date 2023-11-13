4fffa05048e067327153285c526c5674250c058e
Abhishek Gupta <53567693+Abhishek-Gupta-IT@users.noreply.github.com>
2023-11-13 15:58:03 -0500

    Update HeaderComponent.jsx

--- src/frontend/src/App/HeaderComponent.jsx
+++ src/frontend/src/App/HeaderComponent.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Text } from '@fluentui/react-northstar'
+import { Text,Image } from '@fluentui/react-northstar'
+import CotivitiLogo from '../images/Logo_External_CotivitiGPS_Neg1.png'
 
 const HeaderComponent = (props) => {
     
     return (
         <div style={{backgroundColor: "rgb(48, 0, 111)", color: "white", padding: "10px" }}>
-            <Text size="large" content="Business Process Automation Accelerator" />
+            <Image src={CotivitiLogo} alt="Cotiviti" style={{height: 26}} />
+            &nbsp;
+            &nbsp;
+            <Text size="large" content="CHAT WITH DATA" />
             {/* <Header content='Business Process Automation Accelerator' className="header" style={{ fontSize: "20px", color: "white", backgroundColor: "rgb(0, 120, 212)" }} /> */}
             </div>
         // <div className="header" style={{fontFamily: props.theme.siteVariables.bodyFontFamily,  backgroundColor: props.theme.siteVariables.colorScheme.brand.background, color : props.theme.siteVariables.colorScheme.brand.foreground, fontSize: props.theme.siteVariables.fontSizes.larger}}>
