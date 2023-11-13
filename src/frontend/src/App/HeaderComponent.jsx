f7fb6e55a4f4b3dfe0f9ab5a118546ef3e1e1970
Abhishek Gupta <53567693+Abhishek-Gupta-IT@users.noreply.github.com>
2023-11-13 16:06:49 -0500

    Update HeaderComponent.jsx

--- src/frontend/src/App/HeaderComponent.jsx
+++ src/frontend/src/App/HeaderComponent.jsx
@@ -6,11 +6,16 @@ const HeaderComponent = (props) => {
     
     return (
         <div style={{backgroundColor: "rgb(48, 0, 111)", color: "white", padding: "10px" }}>
-            <Image src={CotivitiLogo} alt="Cotiviti" style={{height: 26}} />
+            <Image src={CotivitiLogo} alt="Cotiviti" style={{height: 36}} />
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
             &nbsp;
             &nbsp;
             <Text size="large" content="CHAT WITH DATA" />
-            {/* <Header content='Business Process Automation Accelerator' className="header" style={{ fontSize: "20px", color: "white", backgroundColor: "rgb(0, 120, 212)" }} /> */}
+            {/* <Header content='Business Process Automation Accelerator' className="header" style={{ fontSize: "24px", color: "white", backgroundColor: "rgb(0, 120, 212)" }} /> */}
             </div>
         // <div className="header" style={{fontFamily: props.theme.siteVariables.bodyFontFamily,  backgroundColor: props.theme.siteVariables.colorScheme.brand.background, color : props.theme.siteVariables.colorScheme.brand.foreground, fontSize: props.theme.siteVariables.fontSizes.larger}}>
         //     Business Process Automation Accelerator
